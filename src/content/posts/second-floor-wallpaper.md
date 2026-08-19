ad001e00cf6cac733db3fee864ebba701def2b8b
柒拾兆字节 <3311482378@qq.com>
2026-08-20 00:14:18 +0800

    Add second-floor wallpaper gallery

---
+++ src/content/posts/second-floor-wallpaper.md
@@ -0,0 +1,14 @@
+---
+title: 做了个主页二楼进壁纸的功能
+published: 2026-08-20
+description: 以后就能把图片丢进去了
+tags: [Blog, 更新日志]
+category: 更新日志
+draft: false
+---
+
+# blog直接变成画廊！
+
+虽然抽不动了......等以后有空再塞点吧。
+
+![楼梯](/images/posts/second-floor-stairs.png)
--- src/utils/content-utils.ts
+++ src/utils/content-utils.ts
@@ -2,6 +2,14 @@ import { type CollectionEntry, getCollection } from "astro:content";
 import I18nKey from "@i18n/i18nKey";
 import { i18n } from "@i18n/translation";
 import { getCategoryUrl } from "@utils/url-utils.ts";
+const firstImagePattern = /!\[[^\]]*\]\(\s*(?:<([^>]+)>|([^\s)]+))[^)]*\)|<img\b[^>]*\bsrc\s*=\s*["']([^"']+)["'][^>]*>/i;
+
+export function getFirstImageFromPost(
+	entry: CollectionEntry<"posts">,
+): string {
+	const match = entry.body.match(firstImagePattern);
+	return match?.[1] || match?.[2] || match?.[3] || "";
+}
 
 // // Retrieve posts and sort them by publication date
 async function getRawSortedPosts() {
